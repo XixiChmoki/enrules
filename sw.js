ccb9b13668be11b7edd1b836ca126ed7af837a2f
XixiChmoki <30309180+XixiChmoki@users.noreply.github.com>
2019-01-30 09:35:30 +0300

    sdfzf
    zsfzf

---
+++ sw.js
@@ -0,0 +1,21 @@
+'use strict';
+
+importScripts('sw-toolbox.js');
+
+toolbox.precache([
+	"index.html",
+	"/irregular_verbs/",
+	"/irregular_verbs_table.html",
+	"/irregular_verbs_test.html",
+	"js/irregular_verbs/irrMass.js",
+	"js/irregular_verbs/irregular_verbs_table/irrTable.js",
+	"js/irregular_verbs/irregular_verbs_test/irrMobileTest.js",
+	"js/irregular_verbs/irregular_verbs_test/irrTest.js",
+	"css/styles.css"
+	]);
+
+toolbox.router.get('/img/*', toolbox.cacheFirst);
+
+toolbox.router.get('/*', toolbox.networkFirst, {
+  networkTimeoutSeconds: 5
+});
